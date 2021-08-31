15e5ae06747a39adc0870d45487ac11c5dece6a5
nana0329 <rokunana0329@gmail.com>
2021-08-31 21:03:30 +0900

    課題提出2-2

---
+++ JavaStudy/2-2/src/study/Main.java
@@ -0,0 +1,17 @@
+package study;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// ① 以下のルールに従いUserクラスのインスタンスを生成してください。
+		// 変数名:「user」
+
+		User user = new User("hoge", 123, "abc123");
+		user.printAccountInfo();
+
+		// ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
+		System.out.println();
+	}
+
+}
