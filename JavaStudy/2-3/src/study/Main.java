28295412940783a1b9262dc1ac2b173a2530f14c
nana0329 <rokunana0329@gmail.com>
2021-08-31 21:04:10 +0900

    課題提出2-3

---
+++ JavaStudy/2-3/src/study/Main.java
@@ -0,0 +1,12 @@
+package study;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// ③ Taskクラスのインスタンスを生成し、「doTask()」メソッドを呼び出しなさい。
+		Task ts = new Task();
+		ts.doTask();
+		ts.Calculator();
+	}
+}
